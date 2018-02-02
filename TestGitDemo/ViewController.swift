5c92821784ef333fca1643d5aab1863e027f7f87
Dinesh Reddy <dineshreddy06@gmail.com>
2018-02-02 11:13:24 +0530

    initial commit

---
+++ TestGitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitDemo
+//
+//  Created by capgemini on 02/02/18.
+//  Copyright © 2018 Capgemini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
