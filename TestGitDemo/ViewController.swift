13da8cb4fe0e6be2a5cec475704e9dc3837dc1d9
Murugan <Murugan99122@gmail.com>
2018-02-02 11:26:07 +0530

    branch is added

--- TestGitDemo/ViewController.swift
+++ TestGitDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("hello")
         print("world")
+        print("Branch is added")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
